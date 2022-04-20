b42248d9fa37250fb5579aaecf8ba2ee1fbb36f8
KobelOleh <kobeloleg70@gmail.com>
2022-04-20 16:41:55 +0300

    XYU2

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import telebot
 
+bot = telebot.TeleBot("5036250739:AAHs5zVnWKxXHDUYi2Ii2dCisMx7Erfk2Qg")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  bot.send_message(message.chat.id, 'Саламалейкум', parse_mode='html')
+
+
+bot.polling(none_stop=True)
 
